72614b039d28453cb09f3b5ebcca6ce18243056f
raida <b.raida7@gmail.com>
2025-08-11 19:00:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ app.get('/bbc/rss', async (req, res) => {
     'https://feeds.bbci.co.uk/news/world/rss.xml',
   //  'https://feeds.skynews.com/feeds/rss/world.xml',
     'https://www.aljazeera.com/xml/rss/all.xml',
-    'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+ //   'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
     'https://www.lemonde.fr/rss/une.xml'
   ];
 
