fd81d02546ce8f6e786eda31250e3fe1f1cf7691
raida <b.raida7@gmail.com>
2025-08-13 15:01:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -221,9 +221,9 @@ app.get('/bbc/rss', async (req, res) => {
   const sources = [
     'https://feeds.bbci.co.uk/news/world/rss.xml',
     'https://feeds.skynews.com/feeds/rss/world.xml',
-    'https://www.aljazeera.com/xml/rss/all.xml'
+    'https://www.aljazeera.com/xml/rss/all.xml',
   // 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-   // 'https://www.lemonde.fr/rss/une.xml'
+    'https://www.lemonde.fr/rss/une.xml'
   ];
 
   try {
