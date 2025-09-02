f9432f50c22adad9b1168395299ad10bc4b384fd
Name <b.raida7@gmail.com>
2025-09-02 16:32:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -356,7 +356,7 @@ app.get('/bbc/rss', async (req, res) => {
    // 'https://feeds.skynews.com/feeds/rss/world.xml',
   //  'https://news.un.org/feed/subscribe/en/news/all/rss.xml', 
     // 'https://ir.thomsonreuters.com/rss/sec-filings.xml?items=15', 
-    'https://www.aljazeera.com/xml/rss/all.xml', 
+ //   'https://www.aljazeera.com/xml/rss/all.xml', 
     // 'https://www.icc-cpi.int/rss/news/all', 
   //  'https://www.rsfjournal.org/rss/current.xml',
    'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
